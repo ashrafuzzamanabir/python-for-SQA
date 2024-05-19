c6d333d1bb0ba5581ce5bb47a43367c556c4f87d
ashrafuzzamanabir <lemplifeab@gmail.com>
2024-05-19 22:52:10 +0600

    exception testing

---
+++ PyTest/tests/test_zero.py
@@ -0,0 +1,32 @@
+# content of test_sample.py
+import pytest
+def test_one_plus_one():
+    """
+        Test the equality of the sum of 1 and 1 with 2.
+
+        This function does not take any parameters and does not return anything.
+        It raises an AssertionError if the sum of 1 and 1 is not equal to 2.
+    """
+    assert 1+1==2
+
+def test_one_plus_two():
+    """
+    Test the equality of the sum of 2 and 1 with 3.
+
+    This function does not take any parameters and does not return anything.
+    It raises an AssertionError if the sum of 2 and 1 is not equal to 3.
+    """
+    a=2
+    b=1
+    c=3
+    assert a+b==c
+
+
+def test_devided_by_zero():
+    with pytest.raises(ZeroDivisionError) as e:
+        num = 1/0
+    
+    assert "division by zero" in str(e.value)
+
+    
+
